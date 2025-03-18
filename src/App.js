ed4fddc8dc5679682669d576d36710730a421d54
Prabhas2005 <prabhaschandra2005@gmail.com>
2025-03-18 12:24:07 +0530

    new 1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
+import "./App.css" 
 import Head from "./components/Header";
 import Product from "./components/Products"
 import Footer from "./components/Footer"
+import { BrowserRouter,Route,Routes } from "react-router-dom"
 function App() {
   //let name="John"
   return (
