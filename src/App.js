3d9c38bc2c3c377d424546e77aceffc6053ebfbf
Prabhas2005 <prabhaschandra2005@gmail.com>
2025-03-20 09:25:24 +0530

    new2

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
-import "./App.css" 
-import Head from "./components/Header";
-import Product from "./components/Products"
-import Footer from "./components/Footer"
-import { BrowserRouter,Route,Routes } from "react-router-dom"
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./components/Header";
+import Products from "./components/Products";
+import Footer from "./components/Footer";
+import Login from "./components/Login";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 function App() {
-  //let name="John"
   return (
-    // <>
-    //   <h1>Hello {props.name}. I'm are {props.age}</h1>
-    //   <p>This is a me.</p>
-    // </>
     <div>
-      <Head />
-
-      <Product/>
-      <Footer/>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route index element={<Products />}></Route>
+          <Route path="products" element={<Products />}></Route>
+          <Route path="login" element={<Login />}></Route>
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
