1188e1d1b538b423b0accdfbb87259ae36015f87
Prabhas2005 <prabhaschandra2005@gmail.com>
2025-04-01 11:47:56 +0530

    commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,34 @@
 import "./Header.css";
 import { Link } from "react-router-dom";
-import { appContext } from "../App";
 import { useContext } from "react";
+import { appContext } from "../App";
 export default function Header() {
-  const {user,setUser} = useContext(appContext)
+  const { user, setUser } = useContext(appContext);
   return (
     <div className="App-Header-Row">
-      <h1> React Store</h1>
-      <div>
-        <Link to="products" className="App-Header-Link" >Home</Link>
-        <Link to="cart" className="App-Header-Link">Cart</Link>
-        {user.name === "" ? (<Link to="login" className="App-Header-Link">Login</Link>)
-        :(<Link to="login" className="App-Header-Link">Logout</Link>)}
-        {/* <Link to="login" className="App-Header-Link">Login</Link>
-        <Link to="login" className="App-Header-Link">Logout</Link> */}
+      <h1>My React Store</h1>
+      <div className="App-Header-Links">
+        <Link to="products" className="App-Header-Link">
+          Home
+        </Link>
+        <Link to="cart" className="App-Header-Link">
+          Cart
+        </Link>
+        {user.name === "" ? (
+          <Link to="login" className="App-Header-Link">
+            Login
+          </Link>
+        ) : (
+          <Link
+            to="login"
+            className="App-Header-Link"
+            onClick={() =>
+              setUser({ ...user, name: "" , email: "", password: "" })
+            }
+          >
+            Logout
+          </Link>
+        )}
       </div>
     </div>
   );
